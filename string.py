bc150074950c478d28d14a258ca6926cc6512b7b
Muhammad Rasel <rasel18603@gmail.com>
2025-08-10 10:56:51 +0600

    introduction to python programming

---
+++ string.py
@@ -0,0 +1,32 @@
+
+name = "Muhammad Rasel"
+print(name)
+print(name[2:8])
+
+#uppercase string
+print(name.upper())
+
+#uppercase string
+print(name.lower())
+
+#remove white space from the begining and the end of a string
+print(name.strip())
+
+#replace char or sting with another char or string from string
+print(name.replace("Muhammad","MD"))
+
+#split a string in two parts 
+print(name.split(' '))
+
+# varible print in a string
+age = 25
+print("my age is",age)
+print(f"my age is {age}")
+print(f"I have {age/2:.2f} taka")
+
+#print 2 decimal number
+x=3
+print(f"{x:.2f}")
+print(round(x,2))
+
+
