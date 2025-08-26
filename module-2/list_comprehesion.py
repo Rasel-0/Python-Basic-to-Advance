128c1d1242614a96ba5ec4f34178d4b5a2874a51
Muhammad Rasel <rasel18603@gmail.com>
2025-08-26 12:34:32 +0600

    new folder created

---
+++ module-2/list_comprehesion.py
@@ -0,0 +1,9 @@
+num = [4,45,3,7,8,24,11,43,6,23]
+odd = []
+for x in num :
+    if x % 2 == 1 :
+       odd.append(x) 
+# odd.sort()
+print(odd)
+print( [x for x in num if x % 2 == 1])
+print(list(filter(lambda x : x % 2 == 1, num)))
